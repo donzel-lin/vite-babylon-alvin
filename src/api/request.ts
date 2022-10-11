81b810e43ef63823e3e278b09e30d8a36941d6ad
alvin <alvin@alvin.com>
2022-10-11 10:29:07 +0800

    修改 eslint 报错问题

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,26 +1,22 @@
-import axios from "axios";
-
+import axios from 'axios'
 
 const request = axios.create({
-    withCredentials: true,
-    baseURL: '', // 请求地址
+  withCredentials: true,
+  baseURL: '' // 请求地址
 })
 
 // 错误处理
-const errorHandle = (error: any) => {
-    console.log(error)
+const errorHandle = (error: any): void => {
+  console.log(error)
 }
 // 请求拦截
 request.interceptors.request.use(config => {
-    console.log(config, 'config')
-    return config
+  console.log(config, 'config')
+  return config
 }, errorHandle)
 // 响应拦截
 request.interceptors.response.use(response => {
-    return response
+  return response
 }, errorHandle)
 
-
 export default axios
-
-
