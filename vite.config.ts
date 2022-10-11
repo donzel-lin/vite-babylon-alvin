bcf6f9aecd0e253732ef0010217d49e85aa91587
alvin <alvin@alvin.com>
2022-10-11 10:41:13 +0800

    添加 开发构建校验

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    eslint(),
     AutoImport({
       resolvers: [ElementPlusResolver()]
     }),
